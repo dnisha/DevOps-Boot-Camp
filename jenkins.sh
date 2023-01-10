66481cc980d368cd48b188a88d4b992f7e4f6f64
Deepak Nishad <deepak.n@pinboxinclusion.com>
2023-01-10 15:13:03 +0530

    better jenkins script

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
-if [ -d ~/Documents/DevOps/jenkins-data ] 
+BASEDIR="${PWD}"
+JENKINSDATA="${BASEDIR}/jenkins-data"
+
+echo "Script executed from: ${BASEDIR}"
+echo "Jenkins persistence folder: ${JENKINSDATA}"
+
+if [ -d ${JENKINSDATA} ] 
 then
    echo "Data Directory Already Exist" 
 else
-    mkdir ~/Documents/DevOps/jenkins-data
+    mkdir ${JENKINSDATA}
 fi
 
-podman run --rm -it -p 8080:8080 -p 50000:50000 -v ~/Documents/DevOps/jenkins-data:/var/jenkins_home -u root docker.io/library/jenkins:2.19.1-alpine
+podman run --rm -it -p 8080:8080 -p 50000:50000 -v ${JENKINSDATA}:/var/jenkins_home -u root docker.io/library/jenkins:2.19.1-alpine
 
